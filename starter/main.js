01150a671ea3736b18985c6a6559220b2b1d0118
Woah120 <oluwabukunmigideon@gmail.com>
2022-09-21 06:36:33 -0700

    completed project

---
+++ starter/main.js
@@ -0,0 +1,28 @@
+
+// basic Es5 function declaration
+// function name (){}
+
+var myname = "Djzawurldo"
+
+function popsomething(){
+
+    alert(myname + "I has been coding since 12")
+
+}
+
+
+// concole.log(myname)
+
+console.log(document)                                        
+
+var ourbody = document.querySelector(".ourpagebody")
+
+// ourbody.style.background = "black"
+
+var hamburger = document.querySelector(".hamburger-container")
+
+console.log(hamburger)
+
+function addandremove() {
+    hamburger.classList.toggle('showburger')
+}
